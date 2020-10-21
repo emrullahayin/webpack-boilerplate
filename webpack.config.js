23a3d0d59b9e3c14c067dfa23ae2ae12d12d53dc
Emrullah Ayın <ayinemrullah@gmail.com>
2020-10-21 16:09:07 +0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ const CopyPlugin = require('copy-webpack-plugin');
 const normalizeText = (text) => {
   const firstChar = text.charAt(0);
   const remainingChars = text.substr(1);
-
   return firstChar.toUpperCase() + remainingChars.toLowerCase();
 };
 
